ba79597d920d711872ebfecec3c5c622583a1d2b
antho <Anthonyaldama55@gmail.com>
2024-07-17 23:12:13 -0400

    adapted the values to BLEmanager

--- App-Code/Goniometer/app/src/main/java/com/example/goniometer/BaseActivity.java
+++ App-Code/Goniometer/app/src/main/java/com/example/goniometer/BaseActivity.java
@@ -1,78 +1,81 @@
 package com.example.goniometer;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageButton;
-import android.widget.Toolbar;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
 
 public class BaseActivity extends AppCompatActivity {
 
+    protected Toolbar toolbar;
+    private BluetoothAdapter bluetoothAdapter;
+    protected ImageButton bluetoothButton;
+    private BLEManager bleManager;
 
-protected Toolbar toolbar;
-private BluetoothAdapter bluetoothAdapter;
-protected ImageButton bluetoothbutton;
-private final BroadcastReceiver bluetoothReceiver= new BroadcastReceiver() {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        final String action=intent.getAction();
-        if (action!= null);{
-            if(action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
-                switch (state) {
-                    case BluetoothAdapter.STATE_OFF:
-                    case BluetoothAdapter.STATE_TURNING_OFF:
-                        bluetoothbutton.setImageResource(R.drawable.ic_bluetooth_disabled);
-                        break;
-                    case BluetoothAdapter.STATE_OFF:
-                    case BluetoothAdapter.STATE_TURNING_ON:
-                        ;
-                        bluetoothbutton.setImageResource(R.drawable.ic_bluetooth_connected);
-                        break;
+    private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (action != null) {
+                if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    switch (state) {
+                        case BluetoothAdapter.STATE_OFF:
+                        case BluetoothAdapter.STATE_TURNING_OFF:
+                            bluetoothButton.setImageResource(R.drawable.ic_bluetooth_disabled);
+                            break;
+                        case BluetoothAdapter.STATE_ON:
+                        case BluetoothAdapter.STATE_TURNING_ON:
+                            bluetoothButton.setImageResource(R.drawable.ic_bluetooth_connected);
+                            break;
+                    }
                 }
             }
+        }
+    };
 
-
-
-                }
-            }
-
-
-};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize BLEManager
+        bleManager = new BLEManager(this);
+
+        // Initialize Bluetooth
         BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
 
         registerReceiver(bluetoothReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
-
     }
-protected void setuptoobar(){
+
+    protected void setupToolbar() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        bluetoothbutton = findViewById(R.id.Bluetoothbutton);
-        updatebluetoothbutton();
+        // Enable up navigation
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        bluetoothbutton.setOnClickListener(view->){
+        bluetoothButton = findViewById(R.id.bluetoothButton);
+        updateBluetoothButton();
+
+        bluetoothButton.setOnClickListener(view -> {
             if (bluetoothAdapter.isEnabled()) {
-                bluetoothbutton.disable();
-            }else{
+                bluetoothAdapter.disable();
+            } else {
                 bluetoothAdapter.enable();
             }
-            updatebluetoothbutton();
+            updateBluetoothButton();
+        });
     }
 
-    }
     private void updateBluetoothButton() {
         if (bluetoothAdapter.isEnabled()) {
             bluetoothButton.setImageResource(R.drawable.ic_bluetooth_connected);
@@ -81,6 +84,18 @@ protected void setuptoobar(){
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(bluetoothReceiver);
+    }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
