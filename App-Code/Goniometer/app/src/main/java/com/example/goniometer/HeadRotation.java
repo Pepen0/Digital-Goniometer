df284650e5182e3fab1a7e0daa4002b1c546dc36
Sred47 <sarmadsabbagh83@gmail.com>
2024-07-26 16:42:59 -0400

    fixed issue with roll,pitch values

--- App-Code/Goniometer/app/src/main/java/com/example/goniometer/HeadRotation.java
+++ App-Code/Goniometer/app/src/main/java/com/example/goniometer/HeadRotation.java
@@ -1,6 +1,7 @@
 package com.example.goniometer;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
