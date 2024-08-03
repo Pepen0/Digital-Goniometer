489b0c318466c8fc2708ca344b548626f59dee79
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2024-08-02 22:08:12 -0400

    Collected data on yaw/pitch/roll rotation +/- 90 degrees

--- Arduino-Code/Computer_simulation/Data_writer/Data_writer.py
+++ Arduino-Code/Computer_simulation/Data_writer/Data_writer.py
@@ -2,17 +2,17 @@ import serial
 import time
 
 # Configure the serial connection
-ser = serial.Serial("/dev/tty.usbmodem141201", 9600)
+ser = serial.Serial("/dev/cu.usbmodem141201", 9600)
 time.sleep(2)
 
 # Open files for writing
 accel_file = open(
-    "Arduino-Code/Computer_simulation/Data_container/yaw/acceleration.txt", "w"
+    "Arduino-Code/Computer_simulation/Data_container/roll/acceleration.txt", "w"
 )
 gyro_file = open(
-    "Arduino-Code/Computer_simulation/Data_container/yaw/gyroscope.txt", "w"
+    "Arduino-Code/Computer_simulation/Data_container/roll/gyroscope.txt", "w"
 )
-dt_file = open("Arduino-Code/Computer_simulation/Data_container/yaw/dt.txt", "w")
+dt_file = open("Arduino-Code/Computer_simulation/Data_container/roll/dt.txt", "w")
 
 try:
     while True:
