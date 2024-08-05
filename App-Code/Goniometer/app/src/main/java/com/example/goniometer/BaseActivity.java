90d6d40f777bd22a3d37bbf96ec514ccd8fef369
anthoaldama <115322553+anthoaldama@users.noreply.github.com>
2024-08-05 14:24:50 -0400

    ->blutooth sign please work

--- App-Code/Goniometer/app/src/main/java/com/example/goniometer/BaseActivity.java
+++ App-Code/Goniometer/app/src/main/java/com/example/goniometer/BaseActivity.java
@@ -6,7 +6,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 public abstract class BaseActivity extends AppCompatActivity {
     protected ImageView bluetoothStatus;
@@ -63,11 +63,11 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected void updateUI(boolean isConnected) {
         if (bluetoothStatus != null) {
-            if (isConnected) {
-                bluetoothStatus.setImageResource(R.drawable.baseline_bluetooth_connected_24);
-            } else {
-                bluetoothStatus.setImageResource(R.drawable.baseline_bluetooth_disabled_24);
-            }
+            int color = isConnected ? R.color.bluetooth_connected : R.color.bluetooth_disconnected;
+            Log.d("BaseActivity", "Updating Bluetooth status color to: " + getResources().getColor(color));
+            bluetoothStatus.setColorFilter(ContextCompat.getColor(this, color));
+        } else {
+            Log.e("BaseActivity", "Bluetooth status ImageView is null");
         }
     }
 
